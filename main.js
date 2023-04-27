112b258f7ff2d737459c2ba29414d10cf6d0541f
lemanehedova <127186173+lemanehedova@users.noreply.github.com>
2023-04-26 21:57:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var text= prompt("metn daxil edin ");
+var char=prompt("Herf daxil edin ")
+let count =0;
+for(i=0; i< text.length;i++){
+    if (text[i]==="a"){
+        count++;
+    }
+}
+alert(count);
